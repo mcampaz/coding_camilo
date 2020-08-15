5c2d994cf22799abe09377d63b0f4403fa36b31d
Michael Yesid Urrea <michaelu@grupolistos.net>
2020-08-14 22:55:40 -0500

    cambio diseño CRUD

--- cliente/src/components/lista.js
+++ cliente/src/components/lista.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 import { gql, useQuery } from '@apollo/client';
 
 const MOSTRAR_USUARIO = gql`
-
     query{
         usuarios{
         _id,
@@ -16,7 +16,6 @@ const MOSTRAR_USUARIO = gql`
         Rol
         }
     }
-
 `;
 
 function Lista() {
@@ -26,16 +25,37 @@ function Lista() {
     if (error) return `Error! ${error.message}`;
   
     return (
-
         <Container>
+            <header className="mt-2">
+                <h2>CRUD Data Base</h2>
+            </header>
             <section className="mt-4">
                 <div className="row justify-content-center">
-                    <div className="col-6">
-                    <ListGroup>
-                        {data.usuarios.map(user=> (
-                            <ListGroup.Item key={user.Username}>{user.FirstName} {user.LastName}: {user.Rol } {user.Username}</ListGroup.Item>
-                        ))}
-                    </ListGroup>
+                    <div className="col-12">
+                    <Table hover>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                                <th>Rol</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.usuarios.map((user, i) => (
+                                <tr key={i}>
+                                    <td>{i}</td>
+                                    <td>{user.FirstName}</td>
+                                    <td>{user.LastName}</td>
+                                    <td>{user.Username}</td>
+                                    <td>{user.Rol}</td>
+                                    <td><Button variant="warning" className="mr-3">Edit</Button><Button variant="danger">Del</Button></td>
+                                </tr>
+                             ))}
+                        </tbody>
+                        </Table>
                     </div>
                 </div>
             </section>
