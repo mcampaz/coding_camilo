3a02602a08d950de568142d603324296dbf5b6e8
1KgAzucarMorena <potes1997@hotmail.es>
2020-08-22 19:37:01 -0500

    Cliente: se agrego la consulta de servicios a la vista de servicios

--- cliente/src/components/servicios.js
+++ cliente/src/components/servicios.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "./navBar";
 import AuthContext from "../context/auth-context";
 import Container from "react-bootstrap/Container";
@@ -6,78 +6,64 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import ImgServicios from "../assets/img/serviciosHeader.jpg";
+import { gql, useQuery } from "@apollo/client";
 
-export default class Servicios extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {(context) => {
-          if (context.token) {
-            return (
-              <>
-                <Navbar />
-                <header>
-                  <div className="row text-center mt-2">
-                    <div className="col-12">
-                      <h1>Nuestros servicios</h1>
-                    </div>
-                    <div className="col-12">
-                      <Image
-                        src={ImgServicios}
-                        width="100%"
-                        height="250"
-                        fluid
-                      />
-                    </div>
-                  </div>
-                </header>
-                <Container>
-                  <section className="mt-4">
-                    <div className="row">
-                      <div className="col-12 mt-3">
-                        <Card>
-                          <Card.Body>
-                            <Card.Title>Servicio 1</Card.Title>
-                            <Card.Text>
-                              With supporting text below as a natural lead-in to
-                              additional content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                          </Card.Body>
-                        </Card>
-                      </div>
-                      <div className="col-12 mt-3">
-                        <Card>
-                          <Card.Body>
-                            <Card.Title>Servicio 2</Card.Title>
-                            <Card.Text>
-                              With supporting text below as a natural lead-in to
-                              additional content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                          </Card.Body>
-                        </Card>
-                      </div>
-                      <div className="col-12 mt-3">
-                        <Card>
-                          <Card.Body>
-                            <Card.Title>Servicio 3</Card.Title>
-                            <Card.Text>
-                              With supporting text below as a natural lead-in to
-                              additional content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                          </Card.Body>
-                        </Card>
-                      </div>
-                    </div>
-                  </section>
-                </Container>
-              </>
-            );
-          }
-        }}
-      </AuthContext.Consumer>
-    );
+const MOSTRAR_SERVICIOS = gql`
+  query {
+    servicios {
+      _id
+      Title
+      Description
+    }
   }
+`;
+
+function Servicios() {
+  const { loading, error, data } = useQuery(MOSTRAR_SERVICIOS);
+
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
+
+  return (
+    <AuthContext.Consumer>
+      {(context) => {
+        if (context.token) {
+          return (
+            <>
+              <Navbar />
+              <header>
+                <div className="row text-center mt-2">
+                  <div className="col-12">
+                    <h1>Nuestros servicios</h1>
+                  </div>
+                  <div className="col-12">
+                    <Image src={ImgServicios} width="100%" height="250" fluid />
+                  </div>
+                </div>
+              </header>
+              <Container>
+                <section className="mt-4">
+                  <div className="row">
+                    {data.servicios.map((servicio, i) => (
+                      <div key={i} className="col-12 mt-3">
+                        <Card>
+                          <Card.Body>
+                            <Card.Title>{servicio.Title}</Card.Title>
+                            <Card.Text>{servicio.Description}</Card.Text>
+                            <Button variant="primary">Ver mas</Button>
+                          </Card.Body>
+                        </Card>
+                      </div>
+                    ))}
+                  </div>
+                </section>
+              </Container>
+            </>
+          );
+        }
+      }}
+    </AuthContext.Consumer>
+  );
 }
+
+export default Servicios;
